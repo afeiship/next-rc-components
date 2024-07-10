b3a490dc84bd515030f9989fa99ad1b8c0e3983c
afeiship <1290657123@qq.com>
2024-07-10 23:01:22 +0800

    Release 1.0.4

--- index.d.ts
+++ index.d.ts
@@ -3,11 +3,14 @@ type RcmTypeProps = import('@jswork/react-condition-manager').ReactConditionMana
 type RsmTypeProps = import('@jswork/react-status-manager').ReactStatusManagerProps;
 type ListTypeProps = import('@jswork/react-list').ReactListProps;
 
+// tobe implemented by user
+interface NxRcComponents {}
+
 interface NxStatic {
   $rc: {
     ife: React.ComponentType<IfeTypeProps>;
     rcm: React.ComponentType<RcmTypeProps>;
     rsm: React.ComponentType<RsmTypeProps>;
     list: React.ComponentType<ListTypeProps>;
-  };
+  } & NxRcComponents;
 }
